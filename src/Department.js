cb16a9ee20980e77345b3fcf6e1cdbaf1602a5af
Ven Ra <73223734+venra0712@users.noreply.github.com>
2024-02-13 16:06:24 +0800

    Almost Done

--- src/Department.js
+++ src/Department.js
@@ -38,14 +38,72 @@ export class Department extends Component {
         });
     }
 
-    EditClick(dep) {
+    editClick(dep) {
         this.setState({
             modalTitle: "Edit Department",
-            DepartmentId: dep.DepartmentId,
-            DepartmentName: dep.DepartmentName
+            DepartmentId: dep.departmentId,
+            DepartmentName: dep.departmentName
         });
     }
 
+    createClick(){
+        fetch(variables.API_URL + 'department', {
+            method: 'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                DepartmentName:this.state.DepartmentName
+            })
+        })
+        .then(res=> res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+    updateClick(){
+        fetch(variables.API_URL + 'department', {
+            method: 'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                departmentId:this.state.DepartmentId,
+                departmentName:this.state.DepartmentName
+            })
+        })
+        .then(res=> res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+            fetch(variables.API_URL +'department/'+id, {
+                method: 'DELETE',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                }
+            })
+            .then(res=> res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            })
+        }
+    }
     render() {
         const {
             departments,
@@ -53,6 +111,9 @@ export class Department extends Component {
             DepartmentId,
             DepartmentName
         } = this.state;
+        
+                 
+        console.log(departments)
         return (
             <div>
                 <button type="button" className="btn btn-primary m-2 float-end"
@@ -61,11 +122,11 @@ export class Department extends Component {
                     onClick={() => this.addClick()}>
                     Add Department
                 </button>
-                <table className="table table-striped">
+                <table className="table">
                     <thead>
                         <tr>
                             <th>
-                                DepartmentId
+                                DepartmentID
                             </th>
                             <th>
                                 DepartmentName
@@ -77,9 +138,9 @@ export class Department extends Component {
                     </thead>
                     <tbody>
                         {departments.map(dep =>
-                            <tr key={dep.departmentid}>
-                                <td>{dep.departmentid}</td>
-                                <td>{dep.departmentname}</td>
+                            <tr key={dep.departmentId}>
+                                <td>{dep.departmentId}</td>
+                                <td>{dep.departmentName}</td>
                                 <td>
                                     <button className="btn btn-light me-1" data-bs-toggle="modal"
                                         data-bs-target="#exampleModal"
@@ -90,9 +151,7 @@ export class Department extends Component {
                                         </svg>
                                     </button>
                                     <button className="btn btn-light me-1"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#exampleModal"
-                                        onClick={() => this.editClick()}>
+                                    onClick={()=> this.deleteClick(dep.departmentId)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
@@ -103,14 +162,12 @@ export class Department extends Component {
                         )}
                     </tbody>
                 </table>
-
-                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModal" aria-hidden="true">
-                    <div className="modal=dialog modal-lg modal-dialog-centered">
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg modal-dialog-centered">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title">{modalTitle}</h5>
+                                <h5 className="modal-title" id="exampleModalLabel">{modalTitle}</h5>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
-
                                 </button>
                             </div>
                             <div className="modal-body">
@@ -120,12 +177,19 @@ export class Department extends Component {
                                         onChange={this.changeDepartmentName} />
                                 </div>
 
-                                {DepartmentId == 0 ?
-                                    <button type="button" className="btn btn-primary float-start">Create
+                                {DepartmentId === 0 ?
+                                    <button type="button" 
+                                    className="btn btn-primary float-start"
+                                    onClick={()=>this.createClick()}
+                                    >Create
                                     </button> : null}
 
-                                {DepartmentId != 0 ?
-                                    <button type="button" className="btn btn-primary float-start">Update
+                                {DepartmentId !== 0 ?
+                                    <button type="button" 
+                                    className="btn btn-primary 
+                                    float-start"
+                                    onClick={()=>this.updateClick()}
+                                    >Update
                                     </button> : null}
                             </div>
                         </div>
